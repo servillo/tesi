4ec4fa92be2a65aaf2afa71f2f6c6bae6ed20b88
paolo <servillopaolo@gmail.com>
2018-08-17 16:16:50 +0200

    - testjl

--- test.jl
+++ test.jl
@@ -1,14 +1,39 @@
-using Gallium
-include("LTGA.jl")
-using LTGA
+include("includes.jl")
 
-@time for i = 1:1
-    runGA(1, 12, 16, "lt", 1000000, 3.0 , 0.0 )
-    resetGA()
+using LocalSearch
+using Decoder
+using LTGA
+using Gallium
+
+runGA(1, 56, 20, "mp", 100000, 0.0, 0.0)
+
+LTGA.number_of_generations
+
+sum(LONutility.LON)
+
+function getEdges(LON)
+    dims, dummy = size(LON)
+    str = ""
+    for from = 1:dims
+        for to = 1:dims
+            if LON[from, to] > 0
+                str *= "edges from $(string(from)) to $(string(to)) : $(string(LON[from, to])) \n"
+            end
+        end
+    end
+    return str
 end
 
 
+print(a)
+println(getEdges(LONutility.LON))
 
-# TODO: think of a way to correctly initialize ltga
+decodeOptimum(16383, 56, 1) == trues(56)
+
+MAX_SAFE_PROBLEM_SIZE  = 68
+
 # TODO: fix types
-# TODO: fix LON utilities
+# TODO: fix calling convention (order of parameters)
+# TODO: DONE LON utilities for all best and one best
+# TODO: DONE think of a way to correctly initialize ltga
+# TODO: DONE fix LON utilities
