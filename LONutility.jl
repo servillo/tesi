1d31a6b4a01a5f2ccce4ef4517240dd07ab565b6
paolo <servillopaolo@gmail.com>
2018-09-20 18:09:52 +0200

    added node 0 to LON and removed prints

--- LONutility.jl
+++ LONutility.jl
@@ -12,15 +12,15 @@ prb_size = nothing
 unexplored = 0
 
 function destroy()
-    isConstructed = false
-    unexplored = 0
-    return println("LON reset...")
+    global isConstructed = false
+    global unexplored = 0
+    println("LON reset...")
 end
 
 function constructLON( problem_index, number_of_parameters )
     if !isConstructed
         range = getAllOptimaIndexes(problem_index, number_of_parameters)
-        nodes = range[end]
+        nodes = range[end] + 1
         global LON = Array{Int64}(nodes,nodes)
         LON .= 0
         global isConstructed = true
@@ -35,7 +35,7 @@ end
 function placeEdges( best_prev_solution_indexes, best_solution_indexes)
     for from in best_prev_solution_indexes
         for  to in best_solution_indexes
-            LON[ from , to ] += 1
+            LON[ from + 1 , to + 1 ] += 1
         end
     end
     # LON[best_prev_solution_indexes, best_solution_indexes] = -1
@@ -54,13 +54,13 @@ function getUnexploredInitialPopulation(problem_index, population_size, number_o
     const population = Array{Bool}(population_size, number_of_parameters)
     if toSample < population_size
         for i = toSample + 1: population_size
-            population[i,:] = decodeOptimum(initialOptimaCoded[rand(1:toSample)], number_of_parameters, problem_index )
-            println("added ", codeOptimum(population[i,:], problem_index))
+            population[i,:] = decodeOptimum(rand(getAllOptimaIndexes(problem_index, number_of_parameters)), number_of_parameters, problem_index )
+            # println("added ", codeOptimum(population[i,:], problem_index))
         end
     end
     for i = 1:toSample
         population[i,:] = decodeOptimum(initialOptimaCoded[i], number_of_parameters, problem_index )
-        println("added ", codeOptimum(population[i,:], problem_index))
+        # println("added ", codeOptimum(population[i,:], problem_index))
     end
     return population
 end
