4090fe3d0d929676564a8a3dd52febe0f0cb6879
paolo <servillopaolo@gmail.com>
2018-09-23 19:20:15 +0200

    test env

--- test.jl
+++ test.jl
@@ -11,10 +11,25 @@ using Gadfly # export plot to browser
 using Compose
 using GraphPlot
 
-runGA(1, 56, 20, "mp", 100000, 0.0, 0.0)
+function exploreLandscape(problem_index::Int64)
+    problem_index == 1 ? number_of_parameters = 40 : number_of_parameters = 50
+    return exploreLandscape(problem_index, number_of_parameters, 50)
+end
 
+function exploreLandscape(problem_index::Int64, number_of_parameters::Int64, population_size::Int64)
+    LONutility.constructLON(problem_index, number_of_parameters)
+    vtr = number_of_parameters / 4
+    runs = 0
+    fitness_achieved = []
+    while length(LONutility.unexplored) > 0
+        obj = runGA(1, number_of_parameters, population_size, "mp", 100000, vtr, 0.0)
+        push!(fitness_achieved, maximum(obj))
+        runs += 1
+    end
+    resetGA()
+    return (runs,fitness_achieved)
+end
 
-sum(LONutility.LON)
 
 function getEdges(LON)
     dims, dummy = size(LON)
@@ -29,9 +44,58 @@ function getEdges(LON)
     return str
 end
 
+function weightedLON(LON)
+    rows,cols = size(LON)
+    G = SimpleWeightedDiGraph(rows)
+    for i = 1:rows
+        weight = 1
+        if (weight > 1)
+            weight = 0
+        end
+        for j = 1:cols
+            if LON[i,j] != 0
+                SimpleWeightedGraphs.add_edge!(G, i, j, LON[i,j] * weight)
+            end
+        end
+    end
+    return G
+end
+
+function simpleLON(LON)
+    rows,cols = size(LON)
+    G = SimpleDiGraph(rows)
+    for i = 1:rows
+        for j = 1:cols
+            if LON[i,j] != 0
+                LightGraphs.add_edge!(G, i, j)
+            end
+        end
+    end
+    return G
+end
+
+function removeIslands(LON)
+
+end
+runs, fit = exploreLandscape(1, 40, 2)
+
+
+
+deleteat!(Pmin, find( x -> x < 1, Pmin))
+sum(LONutility.LON)
+LONutility.LON
+
+G = simpleLON(LONutility.LON)
+Gw = weightedLON(LONutility.LON)
+P = pagerank(G, 1.0) .* 100
+
+draw(SVG("graph.svg", 30cm, 30cm), gplot(a))
+
+for i in edges(m)
+    println(i)
+end
 
 println(getEdges(LONutility.LON))
-
 
 MAX_SAFE_PROBLEM_SIZE  = 68
 
@@ -41,45 +105,14 @@ MAX_SAFE_PROBLEM_SIZE  = 68
 # TODO: DONE think of a way to correctly initialize ltga
 # TODO: DONE fix LON utilities
 
-
-
-
-
-
-function weightedLON(LON)
-    rows,cols = size(LON)
-    G = SimpleWeightedDiGraph(rows)
-    for i = 1:rows
-        weight = 1/sum(LON[i,:])
-        if (weight > 1)
-            weight = 0
-        end
-        for j = 1:cols
-            if LON[i,j] != 0
-                add_edge!(G, i, j, LON[i,j] * weight)
-            end
+function asd(m)
+    for c = reverse(vertices(m))
+        if indegree(m,c) == 0 && outdegree(m,c) == 0
+            rem_vertex!(m,c)
         end
     end
-    return G
 end
 
 
-function simpleLON(LON)
-    rows,cols = size(LON)
-    G = SimpleDiGraph(rows)
-    for i = 1:rows
-        for j = 1:cols
-            if LON[i,j] != 0
-                add_edge!(G, i, j)
-            end
-        end
-    end
-    return G
-end
-
-for i in edges(G)
-    println(i)
-end
-
-
-draw(SVG("graph.svg", 30cm, 30cm), gplot(a))
+deleteat!(Pmin, find( x -> x < 1, Pmin))
+deleteat!(Pm, find( x -> x < 1, Pm))
