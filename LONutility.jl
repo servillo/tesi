ebcc6c9c6e67e56828ced5d6c507062c606244b4
paolo <servillopaolo@gmail.com>
2018-10-12 18:17:20 +0200

    prevent self links

--- LONutility.jl
+++ LONutility.jl
@@ -68,7 +68,7 @@ function placeEdges( best_prev_solution_indexes::Array{Int64}, best_solution_ind
                 j = vertices(LON)[end]
                 set_prop!(LON, j, :optimum, "$to")
             end
-            if !add_edge!(LON, i, j)
+            if i != j && !add_edge!(LON, i, j)
                 # println("EDGE ALREADY EXISTS!!!")
             end
         end
